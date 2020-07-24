bb301e4f98ef5959a5743188af221459df7ec58d
J Ulian <hudatov@gmail.com>
2020-07-24 16:26:22 +0300

    api search page

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,4 +1,7 @@
-import { useReducer, useEffect } from 'react';
+import {
+  useReducer,
+  useEffect
+} from 'react';
 import axios from 'axios';
 
 const ACTIONS = {
@@ -9,51 +12,78 @@ const ACTIONS = {
 };
 
 const BASE_URL =
-  'https://cors-anywhere.herokuapp.com/https://api.hh.ru/vacancies';
+  'https://api.hh.ru/vacancies';
 
 function reducer(state, action) {
   switch (action.type) {
     case ACTIONS.MAKE_REQUEST:
-      return { loading: true, jobs: [] };
+      return {
+        loading: true, jobs: []
+      };
 
     case ACTIONS.GET_DATA:
-      return { ...state, loading: false, jobs: action.payload.jobs };
+      return {
+        ...state, loading: false, jobs: action.payload.jobs
+      };
 
     case ACTIONS.ERROR:
       return {
         ...state,
         loading: false,
-        error: action.payload.error,
-        jobs: [],
+          error: action.payload.error,
+          jobs: [],
       };
     case ACTIONS.UPDATE_HAS_NEXT_PAGE:
-      return { ...state, hasNextPage: action.payload.hasNextPage };
+      return {
+        ...state, hasNextPage: action.payload.hasNextPage
+      };
     default:
       return state;
   }
 }
 
 export default function useFetchJobs(params, page) {
-  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
+  const [state, dispatch] = useReducer(reducer, {
+    jobs: [],
+    loading: true
+  });
 
   useEffect(() => {
     const cancelToken1 = axios.CancelToken.source();
-    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    dispatch({
+      type: ACTIONS.MAKE_REQUEST
+    });
     axios
       .get(BASE_URL, {
         cancelToken: cancelToken1.token,
-        params: { page: page, per_page: 25, ...params },
+        params: {
+          page: page,
+          per_page: 25,
+          ...params
+        },
       })
       .then((res) => {
-        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data.items } });
+        dispatch({
+          type: ACTIONS.GET_DATA,
+          payload: {
+            jobs: res.data.items
+          }
+        });
         dispatch({
           type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
-          payload: { hasNextPage: res.data.page <= res.data.pages },
+          payload: {
+            hasNextPage: res.data.page <= res.data.pages
+          },
         });
       })
       .catch((e) => {
         if (axios.isCancel(e)) return;
-        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+        dispatch({
+          type: ACTIONS.ERROR,
+          payload: {
+            error: e
+          }
+        });
       });
 
     return () => {
