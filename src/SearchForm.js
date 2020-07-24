06c4c867464b53830878dd41e266732d3750f4bf
J Ulian <hudatov@gmail.com>
2020-07-24 15:24:22 +0300

    finished jobs api page

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -4,7 +4,7 @@ import { Form, Col } from 'react-bootstrap';
 export default function SearchForm({ params, onParamChange }) {
   return (
     <Form className="mb-4">
-      <Form.Row>
+      <Form.Row className="align-items-end">
         <Form.Group as={Col}>
           <Form.Label>Профессия, должность или компания </Form.Label>
           <Form.Control
@@ -14,6 +14,17 @@ export default function SearchForm({ params, onParamChange }) {
             name="text"
           />
         </Form.Group>
+        {/* <Form.Group as={Col} xs="auto" className="ml-2">
+          <Form.Check
+            onChange={onParamChange}
+            value={params.only_with_salary}
+            name="only_with_salary"
+            id="only-with-salary"
+            label="Only With Salary"
+            type="checkbox"
+            className="mb-2"
+          />
+        </Form.Group> */}
       </Form.Row>
     </Form>
   );
